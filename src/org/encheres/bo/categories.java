512ae47eb4348e74d51a09c61652c7a451d4cfe1
CedricDemeer <cedric@demeer.fr>
2021-02-01 15:18:43 +0100

    1er commit

---
+++ src/org/encheres/bo/categories.java
@@ -0,0 +1,19 @@
+package org.encheres.bo;
+
+public class categories {
+private int no_categorie;
+private String libelle;
+public int getNo_categorie() {
+	return no_categorie;
+}
+public void setNo_categorie(int no_categorie) {
+	this.no_categorie = no_categorie;
+}
+public String getLibelle() {
+	return libelle;
+}
+public void setLibelle(String libelle) {
+	this.libelle = libelle;
+}
+
+}
