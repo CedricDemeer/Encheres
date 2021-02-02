1310d9267b85421fd6b574f9bcfd834ff03ba695
pmichel2020 <pmichel2020@DEV-1-EEL-562.ad.campus-eni.fr>
2021-02-02 13:11:05 +0100

    Ajout jointures dans Select_by_id

--- src/org/encheres/dal/ArticleDAOImpl.java
+++ src/org/encheres/dal/ArticleDAOImpl.java
@@ -7,17 +7,18 @@ import org.encheres.bo.ArticleVendu;
 public class ArticleDAOImpl implements ArticleDAO{
 
 	private static final String SELECT_ALL = "SELECT no_article, nom_article FROM ARTICLES_VENDUS";
-	private static final String SELECT_BY_ID = "SELECT nom_article, description,"
-			+ "date_debut_enchere, "
-			+ "date_fin_enchere,"
-			+ "prix_initial,"
-			+ "prix_vente,"
-			+ "no_utilisateur,"
-			+ "no_categorie,"
-			+ "etat_vente,"
-			+ "image"
-			+ "FROM ARTICLES_VENDUS"
-			+ "WHERE no_article=?";
+	
+	private static final String SELECT_BY_ID = "SELECT a.no_article,"
+			+ "nom_article,description,date_debut_enchere,date_fin_enchere,prix_initial,prix_vente, "
+			+ "a.no_utilisateur as no_user, u.pseudo ,a.no_categorie,etat_vente,image,"
+			+ "r.rue as arue,r.code_postal as acp,r.ville as aville, c.libelle,"
+			+ "e.no_utilisateur as ench_no_utilisateur, e.date_enchere, e.montant_enchere "
+			+ "	FROM (((ARTICLES_VENDUS a LEFT JOIN RETRAITS r ON a.no_article = r.no_article) "
+			+ "			LEFT JOIN CATEGORIES c ON c.no_categorie = a.no_categorie)"
+			+ "			LEFT JOIN UTILISATEURS u ON u.no_utilisateur = a.no_utilisateur)"
+			+ "			LEFT JOIN ENCHERES e ON (a.no_article = e.no_article AND e.no_utilisateur = (SELECT TOP(1) ec.no_utilisateur FROM ENCHERES ec WHERE ec.no_article = a.no_article ORDER BY date_enchere DESC))"
+			+ "			WHERE a.no_article=?";
+	
 	private static final String INSERT_ARTICLE = "INSERT INTO ARTICLES_VENDUS (no_article,nom_article,description,date_debut_enchere,date_fin_enchere,prix_initial,prix_vente,no_utilisateur,no_categorie,etat_vente)"
 			+ "			+ \"     VALUES(?,?,?,?,?,?,?,?,?,?)";
 	//a voir pour l'image si on garde 'image' ou non
