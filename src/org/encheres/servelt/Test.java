490f3ebcfdfafff2140ed25de4c2347e49534b27
yrouland2020 <yrouland2020@DEV-1-EEL-557.ad.campus-eni.fr>
2021-02-02 17:32:07 +0100

    servlet et JSP test pour tester les requêtes

--- src/org/encheres/servelt/Test.java
+++ src/org/encheres/servelt/Test.java
@@ -1,6 +1,8 @@
 package org.encheres.servelt;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.encheres.bo.Utilisateur;
+import org.encheres.dal.UtilisateurDAO;
+import org.encheres.dal.UtilisateurDAOJDBCImpl;
 
 /**
  * Servlet implementation class Test
@@ -28,6 +34,11 @@ public class Test extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Utilisateur> utilisateurs=new ArrayList<>();
+		UtilisateurDAO test= new UtilisateurDAOJDBCImpl();
+		utilisateurs=test.selectAll();
+		request.setAttribute("liste", utilisateurs);
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/test.jsp");
 		rd.forward(request, response);
 	}
