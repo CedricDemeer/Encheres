a48c6fa9804ac56e94488133a7792487ab82cfa2
CedricDemeer <cedric@demeer.fr>
2021-02-02 10:24:03 +0100

    MAJ UtilisateurDAO

--- src/org/encheres/dal/UtilisateurDAO.java
+++ src/org/encheres/dal/UtilisateurDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.encheres.bo.Utilisateur;
 
 public interface UtilisateurDAO {
-public void insert(Utilisateur user);
-public void delete(Utilisateur user);
-public List<Utilisateur> selectAll();
-public Utilisateur selectById(int id);
+	public void insert(Utilisateur user);
+	public void delete(Utilisateur user);
+	public List<Utilisateur> selectAll();
+	public Utilisateur selectById(int id);
 
 }
