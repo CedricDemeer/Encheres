f7c7b6271de972ba3d600c536bdb34b47b3c24c6
CedricDemeer <cedric@demeer.fr>
2021-02-03 13:47:42 +0100

    Correction UtilisateurDAOImpl + correction servlet Inscription

--- src/org/encheres/servlet/Inscription.java
+++ src/org/encheres/servlet/Inscription.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.encheres.bll.UtilisateurManager;
+import org.encheres.bo.Utilisateur;
+
 /**
  * Servlet implementation class Inscription
  */
@@ -36,8 +39,26 @@ public class Inscription extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//récup de variable du formulaire
+				String pseudo = request.getParameter("pseudo");
+				String nom = request.getParameter("lastname");
+				String prenom = request.getParameter("firstname");
+				String email = request.getParameter("email");
+				String telephone = request.getParameter("phone");
+				String rue = request.getParameter("street");
+				String code_postal = request.getParameter("zipcode");
+				String ville = request.getParameter("city");
+				String password = request.getParameter("password");
+				//création du utilisateur manager
+				UtilisateurManager UserMgr = new UtilisateurManager();
+				//création de l'utilisateur
+				Utilisateur user = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, password);
+				System.out.println(pseudo+ nom + prenom);
+				//envoi a la BLL
+				UserMgr.AjoutUtilisateur(user);
+				
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/Accueil.jsp");
+				rd.forward(request, response);
 	}
 
 }
